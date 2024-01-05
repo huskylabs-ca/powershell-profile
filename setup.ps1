a585e8af33827866c9b884da7be25465b9bb5350
huskylabs-ca <15446999+huskylabs-ca@users.noreply.github.com>
2024-01-05 16:14:20 -0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,8 @@
-#If the file does not exist, create it.
+
+# Create Powershell profile config file
 if (!(Test-Path -Path $PROFILE.CurrentUserAllHosts -PathType Leaf)) {
     try {
+
         # Detect Version of Powershell & Create Profile directories if they do not exist.
         if ($PSVersionTable.PSEdition -eq "Core" ) { 
             if (!(Test-Path -Path ($env:userprofile + "\Documents\Powershell"))) {
@@ -19,10 +21,27 @@ if (!(Test-Path -Path $PROFILE.CurrentUserAllHosts -PathType Leaf)) {
     catch {
         throw $_.Exception.Message
     }
-}
-else {
+} else {
     Invoke-RestMethod https://raw.githubusercontent.com/huskylabs-ca/powershell-profile/main/profile.ps1 -OutFile $PROFILE.CurrentUserAllHosts
-    Write-Host "Updating PowerShell profile  @ [$($PROFILE.CurrentUserAllHosts)] has been updated."
+    Write-Host "Updating PowerShell profile  @ [$($PROFILE.CurrentUserAllHosts)]."
+}
+
+# Create Starship config file
+if (!(Test-Path -Path $ENV:STARSHIP_CONFIG -PathType Leaf)) {
+    try {
+        if (!(Test-Path -Path "$HOME\.starship\")) {
+                New-Item -Path "$HOME\.starship\" -ItemType "directory"
+            }
+
+        Invoke-RestMethod https://raw.githubusercontent.com/huskylabs-ca/powershell-profile/main/.starship/starship.toml -OutFile $ENV:STARSHIP_CONFIG
+        Write-Host "Installing Starship profile @ [$($ENV:STARSHIP_CONFIG)]."
+    }
+    catch {
+        throw $_.Exception.Message
+    }
+} else {
+    Invoke-RestMethod https://raw.githubusercontent.com/huskylabs-ca/powershell-profile/main/.starship/starship.toml -OutFile $ENV:STARSHIP_CONFIG
+    Write-Host "Updating Starship profile  @ [$($PROFILE.CurrentUserAllHosts)]."
 }
 
 Write-Host "Installing Starship Terminal..."
@@ -31,5 +50,6 @@ winget install -e --accept-source-agreements --accept-package-agreements --id St
 Write-Host "Installing Terminal Icons..."
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 
-Write-Host "Installing PSWindowsUpdate..."
-Install-Module -Name PSWindowsUpdate -Repository PSGallery -Force
+if (-not $ENV:GOTO_PROJECTS_PATH) {
+    Write-Warning "Goto function path environment variable for projects is missing. Please define `$ENV:GOTO_PROJECTS_PATH in an elevated powershell prompt using the following expression `n -> [System.Environment]::SetEnvironmentVariable('GOTO_PROJECTS_PATH','$HOME\Projects', 'User') `n Replace the path if necessary."
+}
