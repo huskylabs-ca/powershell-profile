8e4b9513a2dba9ebb094d3e20683fa7e54171331
Carl Limoges <limogesc@grics.ca>
2025-03-22 15:01:57 -0400

    Added resiliency to powershell initial setup

--- setup.ps1
+++ setup.ps1
@@ -1,56 +1,76 @@
-
 # Create Powershell profile config file
+Write-Host "Checking PowerShell configuration..."
 if (!(Test-Path -Path $PROFILE.CurrentUserAllHosts -PathType Leaf)) {
     try {
 
         # Detect Version of Powershell & Create Profile directories if they do not exist.
         if ($PSVersionTable.PSEdition -eq "Core" ) { 
-            if (!(Test-Path -Path ($env:userprofile + "\Documents\Powershell"))) {
-                New-Item -Path ($env:userprofile + "\Documents\Powershell") -ItemType "directory"
+
+            $path = $env:userprofile + "\Documents\Powershell"
+
+            if (!(Test-Path -Path $path)) {
+                Write-Host "Creating directory for PowerShell Core profile: [$($path)]"
+                New-Item -Path $path -ItemType "directory"
             }
         }
         elseif ($PSVersionTable.PSEdition -eq "Desktop") {
-            if (!(Test-Path -Path ($env:userprofile + "\Documents\WindowsPowerShell"))) {
-                New-Item -Path ($env:userprofile + "\Documents\WindowsPowerShell") -ItemType "directory"
+            
+            $path = $env:userprofile + "\Documents\WindowsPowerShell"
+
+            if (!(Test-Path -Path $path)) {
+                Write-Host "Creating directory for WindowsPowerShell profile: [$($path)]"
+                New-Item -Path $path -ItemType "directory"
             }
         }
 
+        Write-Host "Installing PowerShell profile: [$($PROFILE.CurrentUserAllHosts)]."
         Invoke-RestMethod https://raw.githubusercontent.com/huskylabs-ca/powershell-profile/main/profile.ps1 -OutFile $PROFILE.CurrentUserAllHosts
-        Write-Host "Installing PowerShell profile @ [$($PROFILE.CurrentUserAllHosts)]."
     }
     catch {
         throw $_.Exception.Message
     }
 } else {
-    Invoke-RestMethod https://raw.githubusercontent.com/huskylabs-ca/powershell-profile/main/profile.ps1 -OutFile $PROFILE.CurrentUserAllHosts
-    Write-Host "Updating PowerShell profile  @ [$($PROFILE.CurrentUserAllHosts)]."
+    Write-Host "Updating PowerShell profile: [$($PROFILE.CurrentUserAllHosts)]."
+    Invoke-RestMethod https://raw.githubusercontent.com/huskylabs-ca/powershell-profile/main/profile.ps1 -OutFile $PROFILE.CurrentUserAllHosts    
 }
 
-# Create Starship config file
-if (-not $env:STARSHIP_CONFIG -or !(Test-Path -Path $ENV:STARSHIP_CONFIG -PathType Leaf)) {
-    try {
-        if (!(Test-Path -Path "$HOME\.starship\")) {
-            New-Item -Path "$HOME\.starship\" -ItemType "directory"
-        }
-
-        $ENV:STARSHIP_CONFIG = "$HOME\.starship\starship.toml"
-        Invoke-RestMethod https://raw.githubusercontent.com/huskylabs-ca/powershell-profile/main/.starship/starship.toml -OutFile $ENV:STARSHIP_CONFIG
-        Write-Host "Installing Starship profile @ [$($ENV:STARSHIP_CONFIG)]."
-    }
-    catch {
-        throw $_.Exception.Message
-    }
-} else {
-    Invoke-RestMethod https://raw.githubusercontent.com/huskylabs-ca/powershell-profile/main/.starship/starship.toml -OutFile $ENV:STARSHIP_CONFIG
-    Write-Host "Updating Starship profile  @ [$($PROFILE.CurrentUserAllHosts)]."
-}
-
+# Updates starship to the latest version
 Write-Host "Installing Starship Terminal..."
 winget install -e --accept-source-agreements --accept-package-agreements --id Starship.Starship
 
+# Create Starship config directory, file and environment variable.
+# Updates the config if the file already exists.
+Write-Host "Checking Starship configuration..."
+if (-not $ENV:STARSHIP_CONFIG -or !(Test-Path -Path $ENV:STARSHIP_CONFIG -PathType Leaf)) {
+    try {
+        $path = "$HOME\.starship\"
+        $configFile = $path + "starship.toml"
+
+        if (!(Test-Path -Path $path)) {
+            Write-Host "Creating directory for starship configuration: [$($path)]"
+            New-Item -Path $path -ItemType "directory"
+        }
+
+        Write-Host "Exporting STARSHIP_CONFIG environment variable with value [$($ENV:STARSHIP_CONFIG)]"
+        [Environment]::SetEnvironmentVariable('STARSHIP_CONFIG', $configFile, 'User')
+
+        Write-Host "Installing Starship profile: [$($ENV:STARSHIP_CONFIG)]."
+        Invoke-RestMethod https://raw.githubusercontent.com/huskylabs-ca/powershell-profile/main/.starship/starship.toml -OutFile $ENV:STARSHIP_CONFIG
+    }
+    catch {
+        throw $_.Exception.Message
+    }
+} else {
+    Write-Host "Updating Starship profile: [$($PROFILE.CurrentUserAllHosts)]."
+    Invoke-RestMethod https://raw.githubusercontent.com/huskylabs-ca/powershell-profile/main/.starship/starship.toml -OutFile $ENV:STARSHIP_CONFIG
+}
+
+# Updates terminal icons to the latest version
 Write-Host "Installing Terminal Icons..."
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 
 if (-not $ENV:GOTO_PROJECTS_PATH) {
-    Write-Warning "Goto function path environment variable for projects is missing. Please define `$ENV:GOTO_PROJECTS_PATH in an elevated powershell prompt using the following expression `n -> [System.Environment]::SetEnvironmentVariable('GOTO_PROJECTS_PATH','$HOME\Projects', 'User') `n Replace the path if necessary."
+    Write-Host "Exporting GOTO_PROJECTS_PATH environment variable with value [$($ENV:GOTO_PROJECTS_PATH)]"
+    [Environment]::SetEnvironmentVariable('GOTO_PROJECTS_PATH', '"$HOME\Projects\"', 'User')
+    Write-Host "Override the GOTO_PROJECTS_PATH environment variable value if the path is different than the default value [$(ENV:GOTO_PROJECTS_PATH)]."
 }
